5bde7cde245716b28e5778759ddd6299bd89acaf
Amv239225 <158981824+Amv239225@users.noreply.github.com>
2024-02-16 12:07:14 +0530

    Add files via upload
    new project

--- railbook/railapp/views.py
+++ railbook/railapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login(request):
+    if(request.method=='POST'):
+        return render(request,'login.html')
+    else:
+        return render(request, 'login.html')
 def register(request):
     if(request.method=='POST'):
         username=request.POST['US']
+        data= store(username=US)
+        data.save()
         print(username)
         return render(request,'login.html')
     else:
